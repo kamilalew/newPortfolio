1735413fe4641f07a9764cf770c7371392ae85f1
Kamila <kamila.m.lewicka@gmail.com>
2020-02-06 19:38:46 +0000

    Initial commit

---
+++ new-portfolio/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="box a">
+        <div className="initials">K <span>L</span>
+        </div>
+      </div>
+      <div className="box z">
+        <div className="aboutMe">JUNIOR FRONT END WEB DEVELOPER LOOKING FOR MY FIRST ROLE.
+      FOR MANY YEARS I'VE MANAGED MULTIPLE INTERNATIONAL RESEARCH PROJECTS. NOW IT'S TIME TO CODE !</div>
+      </div>
+      <div className="box c">3</div>
+      <div className="box d">4</div>
+      <div className="box e">5</div>
+      <div className="box f">6</div>
+      <div className="box g">7</div>
+      <div className="box h">8</div>
+      <div className="box i">9</div>
+      <div className="box j">10</div>
+      <div className="box k">10</div>
+      <div className="box l">10</div>
+      <div className="box m">10</div>
+      <div className="box n">10</div>
+      <div className="box o">10</div>
+      <div className="box p">10</div>
+      <div className="box q">10</div>
+      <div className="box r">10</div>
+      <div className="box s">10</div>
+      <div className="box t">10</div>
+      <div className="box u">10</div>
+      <div className="box v">10</div>
+      <div className="box w">10</div>
+      <div className="box x">10</div>
+      <div className="box y">10</div>
+      <div className="box b">10</div>
+    </div>
+  );
+}
+
+export default App;
